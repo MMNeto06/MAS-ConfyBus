ec806a0e469d2aee584d5563ffd644a6454941eb
MMNeto06 <miguelsneto@ua.pt>
2024-12-15 21:14:55 +0000

    empGestão continuação
    falta mais javascript

--- empGestao.js
+++ empGestao.js
@@ -1,45 +1,148 @@
 $(document).ready(function(){
-    var paragens = [];
+    var paragensCoords = [
+        [40.643959, -8.638941], //aveiro
+        [40.600178, -8.666425], //ílhavo
+        [40.552929, -8.678053], //vagos
+        [40.428381, -8.736320], //mira
+        [40.148324, -8.848864], //figueira da foz 
+        [40.725848, -8.366410], //sever do vouga
+        [40.689300, -8.475258], //Albergaria
+        [40.751342, -8.574918], //Estarreja
+        [40.834232, -8.580066], //Valega
+        [40.860998, -8.622156], //Ovar
+    ];
+    var paragensNomes = [
+        "Aveiro",
+        "Camara de Ílhavo" ,
+        "Bombeiros Vagos",
+        "Mira",
+        "Figueira da Foz",
+        "Sever do Vouga",
+        "Albergaria",
+        "Estarreja",
+        "Válega",
+        "Ovar", 
+    ]
 
-    var map = L.map('map').setView([39.521111, -8.089217], 6);
+    var linhasCoords = [
+        [
+            [40.643959, -8.638941],
+            [40.600178, -8.666425],
+            [40.552929, -8.678053],
+            [40.428381, -8.736320],
+            [40.148324, -8.848864]
+        ],
+        [
+            [40.725848, -8.366410],
+            [40.689300, -8.475258],
+            [40.643959, -8.638941],
+        ],
+        [
+            [40.860998, -8.622156],
+            [40.834232, -8.580066],
+            [40.751342, -8.574918],
+            [40.643959, -8.638941],
+        ]
+    ]
+
+    var linhasNomes = [
+        "Linha 18: Aveiro - Figueira da Foz",
+        "Linha 37: Aveiro - Sever do Vouga",
+        "Linha 33: Aveiro - Ovar"
+    ]
+
+    var linhasCores = [
+        "red",
+        "#57a595",
+        "#57a595",
+    ]
+
+    var novaLInha = [];
+
+    function drawLinhas(coordslst, nomes){
+        for (var i = 0; i< coordslst.length; i++){
+            var polyline = L.polyline(coordslst[i], { color: linhasCores[i] }).bindPopup(nomes[i]).addTo(map);
+            console.log(nomes[i], coordslst[i])
+        }
+    }
+
+    function drawParagens(coords, nomes) {
+        for (var i = 0; i < coords.length; i++) {
+            var marker = L.marker([coords[i][0], coords[i][1]]).bindPopup(nomes[i]).addTo(map);
+            console.log(coords[i], nomes[i])
+        }
+    }
+
+    function onMapClick(e) {
+        console.log(e.latlng);
+        $("#lat").val(e.latlng.lat);
+        $("#lon").val(e.latlng.lng);
+        alert("Coordenadas copiadas para adicionar paragem");
+    }
+    /*
+    function resetMap(){
+        map.clearLayers();
+        drawParagens(paragensCoords, paragensNomes);
+    }*/
+
+    var map = L.map('map').setView([40.642224, -8.643631], 9);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 20,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    var mapParagem = L.map('mapParagem').setView([39.521111, -8.089217], 10);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 20,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(mapParagem);
+    drawParagens(paragensCoords, paragensNomes);
+    drawLinhas(linhasCoords, linhasNomes);
+
+    map.on('click', onMapClick);
+
+    
 
     $('#paragem').click(function () {
-        console.log('adiconar nova paragem');
+        console.log('adicionar nova paragem');
     });
 
-    $("#paragemModal").modal.on('shown.bs.modal', function () {
-        setTimeout(function () {
-            mapParagem.invalidateSize();
-        }, 1);
+    $("#linha").click(function (){
+        console.log("adicionar nova linha");
+        const $container = $("#paragensChecks") 
+        $container.empty();
+
+        for(var i=0; i<paragensNomes.length; i++){
+            const check = `
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="${i}" id="paragem${i}" name="paragem"/>
+                    <label class="form-check-label" for="paragem${i}">
+                        <small>${paragensNomes[i]}</small>
+                    </label>
+                </div>
+            `;
+            $container.append(check);
+        }
+    })
+
+    // Seleciona todos os checkboxes com o nome "paragem"
+    const checkboxes = document.querySelectorAll('input[name="paragem"]');
+
+    // Adiciona o listener a cada checkbox
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', event => {
+            console.log(`Checkbox com valor ${event.target.value} foi ${(event.target.checked ? 'selecionado' : 'deselecionado')}.`);
+        });
     });
-
-    function onMapClick(e) {
-        alert("You clicked the map at " + e.latlng)
-        console.log(e.latlng);
-    }
-
-    map.on('click', onMapClick)
-    mapParagem.on('click', onMapClick);
 
 
     $('#addParagem').click(function () {
         var lat = $("#lat").val();
         var lon = $("#lon").val();
-        var name = $("#name").val();
+        var name = $("#nameParagem").val();
 
         if (parseFloat(lat) && parseFloat(lon) && name != "") {
             console.log(lat, lon, name);
+            var novaParagem = [parseFloat(lat), parseFloat(lon)]
+            paragensCoords.push(novaParagem);
+            paragensNomes.push(name);
             $("#paragemModal").modal('hide');
+            L.marker(novaParagem).bindPopup(name).addTo(map);
         }
         else {
             alert("Faltam dados para adicionar a paragem")
