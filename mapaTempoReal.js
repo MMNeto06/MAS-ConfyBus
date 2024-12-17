e5be1838aa346513538948fbdfba3ba866cb9bbc
MMNeto06 <miguelsneto@ua.pt>
2024-12-17 21:19:53 +0000

    mapa tempo real completo

--- mapaTempoReal.js
+++ mapaTempoReal.js
@@ -1,6 +1,34 @@
-var marker;
-var polyline;
+var map = L.map('map').setView([39.521111, -8.089217], 6);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
+
+var paragensCoords = [
+    [40.643959, -8.638941], //aveiro
+    [40.600178, -8.666425], //ílhavo
+    [40.552929, -8.678053], //vagos
+    [40.428381, -8.736320], //mira
+    [40.148324, -8.848864], //figueira da foz 
+    [40.725848, -8.366410], //sever do vouga
+    [40.689300, -8.475258], //Albergaria
+    [40.751342, -8.574918], //Estarreja
+    [40.834232, -8.580066], //Valega
+    [40.860998, -8.622156], //Ovar
+];
+var paragensNomes = [
+    "Aveiro",
+    "Camara de Ílhavo",
+    "Bombeiros Vagos",
+    "Mira",
+    "Figueira da Foz",
+    "Sever do Vouga",
+    "Albergaria",
+    "Estarreja",
+    "Válega",
+    "Ovar",
+]
 var linhasCoords = [
     [
         [40.643959, -8.638941],
@@ -28,29 +56,28 @@ var linhasNomes = [
     "Linha 33: Aveiro - Ovar"
 ]
 
+var infoLinha = [
+    ["Aveiro", "Figueira da Foz", "12h50", "Camara de Ílhavo", "2 minutos", "15420e"],
+    ["Aveiro", "Sever do Vouga", "11h00", "Aveiro", "0 minutos", "wer123"],
+    ["Aveiro", "Ovar", "10h30", "Estarreja", "10 minutos", "f3gs21"],
+]
+
 var linhasCores = [
     "red",
     "#57a595",
     "#57a595",
 ]
 
-var linhasIds = [
-    "15420e",
-    "wer123",
-    "f3gs21",
-]
-
-var map = L.map('map').setView([39.521111, -8.089217], 6);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 20,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+var marker = L.marker(paragensCoords[0]).addTo(map);
+var polyline = L.polyline(linhasCoords[0], { color: linhasCores[0] }).bindPopup(linhasNomes[0]).addTo(map);
 
 $("#autocomplete").autocomplete({
     source: linhasNomes
 });
 
 $('#button-addon1').click(function(){
+    map.removeLayer(marker)
+    map.removeLayer(polyline)
     console.log("a")
     var linhaNome = $("#autocomplete").val();
     console.log(linhaNome)
@@ -59,7 +86,19 @@ $('#button-addon1').click(function(){
         $("#card").removeClass("d-none");
         var i = linhasNomes.indexOf(linhaNome);
         polyline = L.polyline(linhasCoords[i], {color: linhasCores[i]}).bindPopup(linhasNomes[i]).addTo(map);
-        $("#linha").text(linhasNomes[i])
+        $("#linha").text(linhasNomes[i]);
+
+        $("#origem").text(infoLinha[i][0])
+        $("#destino").text(infoLinha[i][1])
+        $("#hora_prevista").text(infoLinha[i][2])
+        $("#paragem").text(infoLinha[i][3])
+        $("#atraso").text(infoLinha[i][4])
+        $("#id_viagem").text(infoLinha[i][5])
+        
+        console.log(paragensNomes.indexOf[infoLinha[i][3]])
+        var paragemAtual = paragensNomes.indexOf[infoLinha[i][3]];
+        marker = L.marker(paragensCoords[paragemAtual]).addTo(map);
+        
     }
     else{
         $("#card").addClass("d-none");
